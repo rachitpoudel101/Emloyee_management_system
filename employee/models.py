3912fd4726e74db673e03ef07d1ccc7cff95f26d
rachitpoudel101 <rachit300poudel@gmail.com>
2024-06-12 15:16:28 +0545

    Make the models to save data.

--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
+class EmployeeDetails(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    empcode = models.CharField(max_length=50)
+    empdept = models.CharField(max_length=20,null=True)
+    designatation = models.CharField(max_length=100)
